5fd39c8eb6df9bacb2e6f121253e422c533ee9e1
simplyDonald <donaldabuah@gmail.com>
2021-11-25 20:59:30 +0000

    delete redundant db schema

--- migrations/02_schema.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS reservations CASCADE;
-
-CREATE TABLE reservations(
-  id INTEGER PRIMARY KEY NOT NULL,
-  start_date DATE NOT NULL,
-  end_date DATE NOT NULL,
-  property_id INTEGER,
-  guest_id INTEGER
-)
